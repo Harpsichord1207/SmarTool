6f2f3884461b2fc3374cab567da558d4cf74dd07
Derek <liutao3@cig.com.cn>
2021-10-10 19:15:51 +0800

    feat: setup finished

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup, find_packages
+
+read_me = pathlib.Path(__file__).parent.joinpath('README.md').read_text()
+
+setup_args = {
+    'name': 'SmarTool',
+    'version': '1.0.0',
+    'description': 'A set of tools that keep Python sweeter.',
+    'long_description': read_me,
+    'long_description_content_type': 'text/markdown',
+    'author': 'Harpsichord',
+    'author_email': 'tliu1217@163.com',
+    'license': 'MPL 2.0',
+    'packages': ['SmarTool'],
+    'package_dir': {'SmarTool': 'src'},
+    'url': 'https://github.com/Harpsichord1207/SmarTool',
+    'install_requires': [],
+    'classifiers': [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Intended Audience :: Developers'
+    ],
+    'keywords': 'Python Util Tool Convenience',
+    'platforms': 'any',
+    'zip_safe': True
+}
+
+setup(**setup_args)
---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+
+def hello():
+    print("Hello, this is SmarTool!")
