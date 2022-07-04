c742e0597aa4ef48fc3e74426cef111d102003b0
Derek <liutao3@cig.com.cn>
2022-07-04 21:53:00 +0800

    finish 1.0.9

--- src/dtutil.py
+++ src/dtutil.py
@@ -8,7 +8,13 @@ class DTUtil:
         if date is None:
             date = datetime.datetime.today()
         target_month = date.month + int(months)
-        return date.replace(year=date.year+target_month // 12, month=target_month % 12)
+        try:
+            return date.replace(year=date.year+target_month // 12, month=target_month % 12)
+        except ValueError:
+            # for date 2/29
+            date = date.replace(day=1)
+            target_month += 1
+            return date.replace(year=date.year+target_month // 12, month=target_month % 12)
 
     @classmethod
     def first_day_of_month(cls, date: datetime.datetime = None):
--- tests/test_dtutil.py
+++ tests/test_dtutil.py
@@ -14,6 +14,9 @@ class TestDTUtil(unittest.TestCase):
         self.assertEqual("2023-04-01", DTUtil.add_month(d, months=12).strftime(self.fmt1))
         self.assertEqual("2023-05-01", DTUtil.add_month(d, months=13).strftime(self.fmt1))
 
+        d = datetime.datetime.strptime("2020-02-29", self.fmt1)
+        self.assertEqual("2022-03-01", DTUtil.add_month(d, months=24).strftime(self.fmt1))
+
     def test_first_day_of_month(self):
         d = datetime.datetime.strptime("2022-04-03", self.fmt1)
         self.assertEqual("2022-04-01", DTUtil.first_day_of_month(d).strftime(self.fmt1))
